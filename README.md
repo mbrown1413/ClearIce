92681013339c3d4597f2335811042ff8da6d6cf5
Michael Brown <michael@msbrown.net>
2018-02-09 08:48:36 -0500

    Project description

---
+++ README.md
@@ -0,0 +1,34 @@
+
+ClearIce: A Static Site Generator
+=================================
+
+A simple extensible static site generator. It uses:
+
+  * [Jinja2](http://jinja.pocoo.org/) template engine
+  * [Markdown](https://daringfireball.net/projects/markdown/) content format
+  * [YAML](http://yaml.org/) for frontmatter (page metadata)
+
+Development Status
+------------------
+
+ClearIce is still in development. Currently, the major lacking features are:
+
+  * Documentation
+  * Theme support
+  * Custom generator extensibility
+  * Command line options
+
+
+Tests
+-----
+
+Running Tests:  (requires `nose` and `coverage`)
+
+    $ nosetests
+
+Coverage report generated in `cover/`.
+
+License
+-------
+
+See `LICENSE.txt`
--- setup.py
+++ setup.py
@@ -2,18 +2,25 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+import sys
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except ImportError:
+    if 'upload' in sys.argv[1:]:
+        print('Package "pypandoc" required to upload.')
+        raise
+    long_description = open('README.md').read()
 
 setup(
     name="clearice",
     version="0.1",
     description="Static site generator that is both simple and flexible.",
     long_description=long_description,
-    #url="",  #TODO
+    url="https://github.com/mbrown1413/ClearIce",
     author="Michael S. Brown",
     author_email="michael@msbrown.net",
     classifiers=[
